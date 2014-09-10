9ee07ff137f3f83180b3a8d7d49f69faa8cef3d5
Lexxizz <Lexxizz@gmail.com>
2014-09-10 22:44:44 +0300

    00

--- Prodcat/admin/language/russian/module/prodcat.php
+++ Prodcat/admin/language/russian/module/prodcat.php
@@ -1,28 +1,41 @@
 <?php
 // Heading
-$_['heading_title']       = 'Товары в категории';
+$_['heading_title_fake']                        = 'OpenRecentSales - Последние продажи';
+$_['heading_title']                             = '<span style="color:#0071A0;font-weight:bold;">OpenRecentSales - Последние продажи</span>'; 
 
 // Text
-$_['text_module']         = 'Модули';
-$_['text_success']        = 'Модуль Товары в категории успешно обновлен!';
-$_['text_content_top']    = 'Верх страницы';
-$_['text_content_bottom'] = 'Низ страницы';
-$_['text_column_left']    = 'Левая колонка';
-$_['text_column_right']   = 'Правая колонка';
-$_['text_cat']   		= 'Выберите категорию ';
-$_['text_desc']   		= 'Вывод описания';
-$_['text_podcat']   = 'Вывод подкатегорий';
-$_['text_subwh']   = 'Размер изображений подкатегорий(Ш х В)';
-$_['text_goods']   = 'Вывод случайных товаров';
-$_['text_goodswh']   = 'Размер изображений случайных товаров(Ш х В)';
-$_['text_num']   = 'Количество случайных товаров товаров';
+$_['text_module']                               = 'Модули';
+$_['text_success']                              = 'Модуль OpenRecentSales успешно обновлен!';
+$_['text_content_top']                          = 'Верх страницы';
+$_['text_content_bottom']                       = 'Низ страницы';
+$_['text_column_left']                          = 'Левая колонка';
+$_['text_column_right']                         = 'Правая колонка';
+$_['tab_module']                                = 'Модуль';
+$_['button_add_menu']                           = 'Добавить модуль';
 
-// Entry
-$_['entry_layout']        = 'Схема:';
-$_['entry_position']      = 'Расположение:';
-$_['entry_status']        = 'Статус:';
-$_['entry_sort_order']    = 'Порядок сортировки:';
+// Entry        
+$_['entry_layout']                              = 'Схема:';
+$_['entry_position']                            = 'Расположение:';
+$_['entry_status']                              = 'Статус:';
+$_['entry_limit']                               = 'Лимит товаров:';
+$_['entry_sort_order']                          = 'Порядок сортировки:';
+$_['entry_image']                               = 'Изображение:';
+$_['entry_title']                               = 'Заголовок модуля:';
+$_['entry_visible']                             = 'Количество одновременно видимых товаров:';
+$_['entry_scroll']                              = 'Товаров прокручивается за раз:';
+$_['entry_auto']                                = 'Интервал автоматической прокрутки в секундах:';
+$_['entry_next']                                = 'Отображение элементов управления:';
+$_['entry_animation']                           = 'Скорость прокрутки в милисекундах:';
+$_['entry_price']                               = 'Отображение цены:';
+$_['entry_customer']                            = 'Отображение имени покупателя:';
+$_['entry_date']                                = 'Отображение даты покупки:';
+$_['entry_type']                                = 'Тип отображения заголовков:';
+$_['entry_type_text']                           = 'Текст';
+$_['entry_type_ico']                            = 'Иконки';
+$_['help_image']                                = '(ширина x высота)';
+$_['help_auto']                                 = '(чтобы выключить автопрокуртку оставьте поле пустым)';
 
 // Error
-$_['error_permission']    = 'У Вас нет прав для изменения модуля Товары в категории!';
+$_['error_permission']                          = 'У Вас нет прав для изменения модуля OpenRecentSales!';
+$_['error_image']                               = 'Заполните поле!';
 ?>
